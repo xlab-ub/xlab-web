9fde98e3e408523c5e53538c22301b27f50aeb90
KiarashS <kiarash.s@hotmail.com>
2022-05-04 22:36:53 +0430

    Fix

--- js/custom.js
+++ js/custom.js
@@ -13,32 +13,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
             var elem1 = document.querySelector('#news-container');
             var html1 = md.render(firstResponse.data);;
-            elem1.innerHTML = html1;
+            //elem1.innerHTML = html1;
 
             console.log(html1);
             var elem2 = document.querySelector('#research-container');
             var html2 = md.render(secondResponse.data);;
-            elem2.innerHTML = html2;
+            //elem2.innerHTML = html2;
 
             var elem3 = document.querySelector('#publication-container');
             var html3 = md.render(thirdResponse.data);;
-            elem3.innerHTML = html3;
+            //elem3.innerHTML = html3;
 
             var elem4 = document.querySelector('#recognition-container');
             var html4 = md.render(forthResponse.data);;
-            elem4.innerHTML = html4;
+            //elem4.innerHTML = html4;
 
             var elem5 = document.querySelector('#services-container');
             var html5 = md.render(fifthResponse.data);;
-            elem5.innerHTML = html5;
+            //elem5.innerHTML = html5;
 
             var elem6 = document.querySelector('#teaching-container');
             var html6 = md.render(sixthResponse.data);;
-            elem6.innerHTML = html6;
+            //elem6.innerHTML = html6;
 
             var elem7 = document.querySelector('#positions-container');
             var html7 = md.render(seventhResponse.data);;
-            elem7.innerHTML = html7;
+            //elem7.innerHTML = html7;
         }))
         .catch(error => console.log(error));
 }, false);
